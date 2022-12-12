c39f5f5875b5d84bba3e93f8ef217d7b423517f7
SankhaYapa <sankhapriyamantha55@gmail.com>
2022-12-12 15:10:52 +0530

    update

--- api/index.js
+++ api/index.js
@@ -7,16 +7,38 @@ const morgan = require("morgan");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
+const multer = require("multer");
+const path = require("path");
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL, () => {
   console.log("Connected to MongoDB");
 });
+
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 //middleware
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploaded successfully");
+  } catch (error) {
+    console.log(error);
+  }
+});
 // app.get("/", (req, res) => {
 //   res.send("Welcome to home page");
 // });
--- api/routes/posts.js
+++ api/routes/posts.js
@@ -49,10 +49,10 @@ router.put("/:id/like", async (req, res) => {
     const post = await Post.findById(req.params.id);
     if (!post.likes.includes(req.body.userId)) {
       await post.updateOne({ $push: { likes: req.body.userId } });
-      res.status(500).json("The post has been liked");
+      res.status(200).json("The post has been liked");
     } else {
       await post.updateOne({ $pull: { likes: req.body.userId } });
-      res.status(500).json("The post has been disliked");
+      res.status(200).json("The post has been disliked");
     }
   } catch (error) {
     res.status(500).json(error);
